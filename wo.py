f1d85641965de04e01066faa2631f3f8e2fa017a
EchoPss <2728196335@qq.com>
2019-05-12 16:21:14 +0800

    拉拉

---
+++ wo.py
@@ -0,0 +1,8 @@
+﻿count=0
+with open(r'D:\python123\text-file-process-EchoPss\text-file-process\log_files\201811113018.log',encoding='utf8') as f:
+    for line in f:
+        str1=line.split('：')[2]
+        str2=str1[:12]
+        if str2=='201811113018':
+            count+=1
+print(count)
